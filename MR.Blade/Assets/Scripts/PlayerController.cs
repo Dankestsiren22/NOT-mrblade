3c50e4dcd20bb9e847ef0fb90838dddc969db942
WEEJIU <Jimmmyshit@gmail.com>
2025-09-16 10:10:43 -0700

    fixed camera, added jump
    yes

--- MR.Blade/Assets/Scripts/PlayerController.cs
+++ MR.Blade/Assets/Scripts/PlayerController.cs
@@ -3,32 +3,31 @@ using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
-    Vector2 cameraRotation;
-    InputAction lookVector;
-    Transform playerCam;
+    
+
+    Camera playerCam;
     Rigidbody rb;
-    Transform camHolder;
+    
     public Vector3 respawnPoint;
 
     float verticalmove;
     float horizontalmove;
+
     public float speed = 5f;
     public float jumpHeight = 10f;
-    public float xsensitvity = 1.0f;
-    public float ysensitvity = 1.0f;
-    public float camrotationlimit = 90.0f;
-
+    public float GroundDetetlenght = .5f;
+    Ray ray;
+    RaycastHit hit;
     public int health = 5;
     public int maxhealth = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        ray = new Ray(transform.position, transform.forward);
         respawnPoint = new Vector3(0, 1, 0);
         rb = GetComponent<Rigidbody>();
-        playerCam = transform.GetChild(0);
-        lookVector = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
-        cameraRotation = Vector2.zero;
-        camHolder = transform.GetChild(0);
+        playerCam = Camera.main;
+  
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -41,24 +40,25 @@ public class PlayerController : MonoBehaviour
             transform.position = respawnPoint;
             health = maxhealth;
         }
-        
+
         //camera rotatiom
         //playerCam.transform.position = camHolder.position;
 
-
-        cameraRotation.x += lookVector.ReadValue<Vector2>().x * xsensitvity;
-        cameraRotation.y += lookVector.ReadValue<Vector2>().y * ysensitvity;
-
-        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -camrotationlimit, camrotationlimit);
-        
-
-        playerCam.rotation = Quaternion.Euler(-cameraRotation.y, cameraRotation.x, 0);
-        transform.localRotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+        Quaternion playerRotaion = playerCam.transform.rotation;
+        playerRotaion.x = 0;
+        playerRotaion.z = 0;
+        transform.rotation = playerRotaion;
         
         // movment system
         Vector3 temp = rb.linearVelocity;
         temp.x = verticalmove * speed;
         temp.z = horizontalmove * speed;
+
+        ray.origin = transform.position;
+        ray.direction = -transform.up;
+        
+
+       
 
         rb.linearVelocity = (temp.x * transform.forward) + (temp.y * transform.up) + (temp.z * transform.right);
     }
@@ -69,6 +69,13 @@ public class PlayerController : MonoBehaviour
         verticalmove = inputAxis.y;
         horizontalmove = inputAxis.x;
       
+    }
+    public void jump()
+    {
+        if(Physics.Raycast(ray, GroundDetetlenght))
+            rb.AddForce(transform.up * jumpHeight);
+     
+            
     }
 
     private void OnTriggerEnter(Collider other)
