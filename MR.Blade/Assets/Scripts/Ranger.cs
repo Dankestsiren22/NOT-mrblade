20e64ecd82ac1c64553fe18de45d601c1765a921
Dankestsiren22 <jimmmyshit@gmail.com>
2025-09-21 18:25:57 -0700

    fixed ai

--- MR.Blade/Assets/Scripts/Ranger.cs
+++ MR.Blade/Assets/Scripts/Ranger.cs
@@ -1,13 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 public class Ranger : MonoBehaviour
 {
     NavMeshAgent agent;
-    private Vector3 Pos;
+    public float speed;
+    public float stopdistance;
+    public GameObject Shot;
+    private Transform player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = GameObject.FindWithTag("player").transform;
         agent = GetComponent<NavMeshAgent>();
 
     }
@@ -15,9 +20,29 @@ public class Ranger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Vector3.Distance(transform.position, player.position) < stopdistance)
+        {
+            agent.destination = Vector3.MoveTowards(transform.position, player.position, -speed);
+        }
+        else if(Vector3.Distance(transform.position, player.position) > stopdistance)
+        {
+            agent.destination = Vector3.MoveTowards(transform.position, player.position, speed);
+        }
+
         
-        
-    }
+
   
-    
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
