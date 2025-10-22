4db2dcc831f8049a6635fb4dbd0037e90df51573
Dankestsiren22 <jimmmyshit@gmail.com>
2025-10-22 10:05:58 -0700

    ADDED KAtana FOR MElle UInts

--- MR.Blade/Assets/Scripts/Melee.cs
+++ MR.Blade/Assets/Scripts/Melee.cs
@@ -5,14 +5,18 @@ public class Melee : MonoBehaviour
     NavMeshAgent agent;
     private Transform player;
     public float SwingRange;
+    
+    public GameObject Katana;
     public float health = 5;
     Reggy Reggy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindWithTag("player").transform;
+        //Katana = GameObject.FindWithTag("Katana").GetComponent<BoxCollider>();
         agent = GetComponent<NavMeshAgent>();
         Reggy = GameObject.FindGameObjectWithTag("Reggy").GetComponent<Reggy>();
+        
     }
 
     // Update is called once per frame
@@ -20,8 +24,11 @@ public class Melee : MonoBehaviour
     {
         agent.destination = GameObject.Find("player").transform.position;
 
-        //if (Vector3.Distance(transform.position, player.position) < SwingRange)
-        //attack
+        if (Vector3.Distance(transform.position, player.position) < SwingRange)
+        {
+           //Katana.BoxCollider.SetActive(true);
+        }
+        
         if (Reggy.St1P1 == true)
         {
             if (health <= 0)
